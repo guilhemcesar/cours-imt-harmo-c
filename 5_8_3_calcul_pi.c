65203ee2cdfd659502465bec27c1a77fe4504979
Guilhem CESAR <guilhem.cesar_dit_faure@open-groupe.com>
2019-01-09 16:12:26 +0100

    creation dossier

--- 5_8_3_calcul_pi.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-void main(){
-    float som = 0;
-    for (float i = 1; i <= 100000; i++) som = som + 1/(i*i);
-    printf("pi = %f", sqrt(6*som));
-}
